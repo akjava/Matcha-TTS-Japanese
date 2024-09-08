fdbe1e59a76118e9b75cc2fd559181e4f4c421a2
Aki <akxucker@gmail.com>
2024-09-08 18:38:15 +0900

    change heckpoint_{epoch:04d}

--- matcha/train.py
+++ matcha/train.py
@@ -104,12 +104,14 @@ def main(cfg: DictConfig) -> Optional[float]:
     :param cfg: DictConfig configuration composed by Hydra.
     :return: Optional[float] with optimized metric value.
     """
+    print(cfg)
     # apply extra utilities
     # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
     utils.extras(cfg)
 
     # train the model
     metric_dict, _ = train(cfg)
+    
 
     # safely retrieve metric value for hydra-based hyperparameter optimization
     metric_value = utils.get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
