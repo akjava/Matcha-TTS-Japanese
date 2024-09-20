671c1ae994ce8ad76c7266251d6dac04a7acb521
Aki <akxucker@gmail.com>
2024-09-20 18:34:46 +0900

    add dev

---
+++ examples/js-esm/matcha_tts_onnx_en_dev.js
@@ -0,0 +1,53 @@
+import { MatchaTTSRaw } from "./matcha_tts_raw.js";
+import { webWavPlay } from "./web_wav_play.js";
+import { arpa_to_ipa } from "./arpa_to_ipa.js";
+import { loadCmudict } from "./cmudict_loader.js";
+import { env,textToArpa} from "./text_to_arpa.js";
+        
+        env.allowLocalModels = true;
+        //for transfomer.js bart-g2p
+        env.localModelPath = "https://akjava.github.io/Matcha-TTS-Japanese/models/";
+        env.cmudictPath = 'https://akjava.github.io/Matcha-TTS-Japanese/dictionaries/cmudict-0.7b'
+        env.backends.onnx.logLevel = "fatal";
+        env.matcha_tts_debug = false
+        let matcha_tts_raw;
+        let cmudict ={};
+        let speaking = false;
+        async function matcha_tts(model_path,text,speed=1.0,tempature=0.5,spk=0) {
+
+            if( model_path == null){
+                //maybe need change to model page
+                model_path = "https://huggingface.co/spaces/Akjava/matcha-tts-onnx-benchmarks/resolve/main/models/matcha-tts/ljspeech_sim.onnx"
+            }
+
+            if (speaking){
+                console.log("speaking return")
+            }
+            speaking = true
+            if(!matcha_tts_raw){
+                matcha_tts_raw = new MatchaTTSRaw()
+                await matcha_tts_raw.load_model(model_path,{ executionProviders: ['webgpu','wasm'] });
+                let cmudictReady = loadCmudict(cmudict,env.cmudictPath)
+                await cmudictReady
+            }else{
+                if (env.matcha_tts_debug){
+                    console.log("session exist skip load model")
+                }
+            }
+           
+            const arpa_text = await textToArpa(cmudict,text)
+            const ipa_text = arpa_to_ipa(arpa_text).replace(/\s/g, "");
+            console.log(ipa_text)
+            const spks = 0
+            
+            const result = await matcha_tts_raw.infer(ipa_text, tempature, speed,spks);
+            
+            if (result!=null){
+                webWavPlay(result)
+               
+            }
+    
+            speaking = false
+        }
+
+        export{matcha_tts,env,cmudict}
